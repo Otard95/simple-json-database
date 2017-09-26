18105b37fa8afdef47d00e387cd855109ecce9c1
Otard95 <stian.myklebostad@getmail.no>
2017-09-26 22:25:14 +0200

    matchTemplate now ignores equals methud

--- bin/util.js
+++ bin/util.js
@@ -13,24 +13,30 @@ var util = {
 
     for (var key in obj) { // for every key on obj
 
-      var temKey = key;
-      // if tem/obj is array default key = 0
-      if (Array.isArray(tem)) {
-        temKey = 0;
-      }
+      if (key != 'equals') { // this is a methud to check if a identical element
+                             // is allready in the table, its not a value to be saved
+                             // and so it will not be in the tem(plate) object either
 
-      if (tem[temKey] == undefined) return false; // key is not in tem
+        var temKey = key;
+        // if tem/obj is array default key = 0
+        if (Array.isArray(tem)) {
+          temKey = 0;
+        }
 
-      if (typeof obj[key] !== typeof tem[temKey]) return false; // val of key is not same type
+        if (tem[temKey] == undefined) return false; // key is not in tem
 
-      if (typeof obj[key] == 'object') { // if type is object use thes function recursively
-        if (!util.matchTemplate(obj[key], tem[temKey])) return false;
-      }
+        if (typeof obj[key] !== typeof tem[temKey]) return false; // val of key is not same type
 
-      if ( Array.isArray(tem) ) {
-        tem.shift();
-      } else {
-        delete tem[key];
+        if (typeof obj[key] == 'object') { // if type is object use thes function recursively
+          if (!util.matchTemplate(obj[key], tem[temKey])) return false;
+        }
+
+        if ( Array.isArray(tem) ) {
+          tem.shift();
+        } else {
+          delete tem[key];
+        }
+
       }
 
     }
