03888f507182563fdca95272f9d1bde0a27dbd5e
Otard95 <stian.myklebostad@getmail.no>
2017-10-23 15:09:29 +0200

    updated template metching

--- bin/util.js
+++ bin/util.js
@@ -12,41 +12,56 @@ var util = {
   // Returns false on mismatch and true on match
   matchTemplate: (obj, tem) => {
 
-    for (var key in obj) { // for every key on obj
+    if (Array.isArray(tem)) {
 
-      if (key != 'equals') { // this is a methud to check if a identical element
-                             // is allready in the table, its not a value to be saved
-                             // and so it will not be in the tem(plate) object either
+      let match = true;
+      obj.forEach((item, i, arr) => {
 
-        var temKey = key;
-        // if tem/obj is array default key = 0
-        if (Array.isArray(tem)) {
-          temKey = 0;
+        if (match) {
+
+          if (typeof item != typeof tem[0]) {
+            match = false;
+            //console.log('in arr type missmatch');
+          } else if (typeof item == 'object') {
+            let clone = JSON.parse(JSON.stringify(tem[0]));
+            //console.log(item, clone);
+            match = util.matchTemplate(item, clone);
+            // if (!match) {
+            //   console.log('obj in arr missmatch');
+            // }
+          }
+
         }
 
-        if (tem[temKey] == undefined) return false; // key is not in tem
+      });
 
-        if (typeof obj[key] !== typeof tem[temKey]) return false; // val of key is not same type
+      return match;
 
-        if (typeof obj[key] == 'object') { // if type is object use thes function recursively
-          if (!util.matchTemplate(obj[key], tem[temKey])) return false;
-        }
+    } else {
 
-        if ( Array.isArray(tem) ) {
-          tem.shift();
-        } else {
+      for (var key in obj) { // for every key on obj
+
+        if (key != 'equals') { // this is a methud to check if a identical element
+                               // is allready in the table, its not a value to be saved
+                               // and so it will not be in the tem(plate) object either
+
+          if (tem[key] == undefined) return false; // key is not in tem
+          //console.log('not undef');
+          if (typeof obj[key] !== typeof tem[key]) return false; // val of key is not same type
+          //console.log('type match');
+          if (typeof obj[key] == 'object') { // if type is object use thes function recursively
+            if (!util.matchTemplate(obj[key], tem[key])) return false;
+            //console.log('inner match');
+          }
+
           delete tem[key];
+
         }
 
       }
 
-    }
+      return (Object.keys(tem).length == 0);
 
-    // if array check length
-    if (Array.isArray(tem)) {
-      return !(tem.length > 0);
-    } else {
-      return !(Object.keys(tem).length > 0);
     }
 
   }
